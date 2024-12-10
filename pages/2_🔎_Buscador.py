b2a0f15b4b05e15344e78ce59b85f8d2afa228de
Yadhielí Enalith Gonzales Palma <enalithnotes@gmail.com>
2024-12-09 23:39:31 -0500

    Update 2_🔎_Buscador.py

--- pages/2_🔎_Buscador.py
+++ pages/2_🔎_Buscador.py
@@ -3,6 +3,7 @@ import pandas as pd #Ya mencionado anteriormente, permite manipular y manejar lo
 import streamlit as st
 import numpy as np #ofrece herramientas que manejan datos númericos
 import function as fl 
+import folium
 
 #PASO 01
 #Cargamos la base de datos de películas
